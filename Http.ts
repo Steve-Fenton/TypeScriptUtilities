9f033afaf240bdd7e19fe6ce96e06e3a8573d9f4
Steve Fenton <info@enhance-php.com>
2013-02-12 15:31:22 +0000

    Update Http.ts
    Added the others!

--- Http.ts
+++ Http.ts
@@ -12,9 +12,13 @@ var someAjaxAction = Http.HttpVerb.GET;
 
 module Http {
     export class HttpVerb {
+        public static CONNECT = 'CONNECT';
+        public static DELETE = 'DELETE';
         public static GET = 'GET';
+        public static HEAD = 'HEAD';
+        public static OPTIONS = 'OPTIONS';
         public static POST = 'POST';
         public static PUT = 'PUT';
-        public static DELETE = 'DELETE';
+        public static TRACE = 'TRACE';
     }
 }
